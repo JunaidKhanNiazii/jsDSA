1db73f1c642ad31f62080f8585dda722d65abbfa
Rhayat1151 <junaidameerkhan555@gmail.com>
2026-06-22 10:01:15 +0500

    leadcode problem best time to buy and sell

--- lecture8/leadcode_best_time_to_buysell.js
+++ lecture8/leadcode_best_time_to_buysell.js
@@ -1,23 +1,44 @@
 // This is working correctly but the time out due to 2 loop 
 
-// prices = [7,1,5,3,6,4]
-// prices = [2,4,1]
-prices = [2, 10, 1, 5]
-let max_profit = 0
-let buy_day = 0
-let sell_day =0
-for (let i=0; i < prices.length; i++)
-{
-    for( let j = i + 1; j< prices.length; j++)
-    {
-        let profit = prices[j] - prices[i]
-        if (prices[i] < prices[j] &  max_profit< profit )
-        {
-            max_profit = profit
-            buy_day = i
-            sell_day = j
-        }
-    }
-}
+// // prices = [7,1,5,3,6,4]
+// // prices = [2,4,1]
+// prices = [2, 10, 1, 5]
+// let max_profit = 0
+// for (let i=0; i < prices.length; i++)
+// {
+//     for( let j = i + 1; j< prices.length; j++)
+//     {
+//         let profit = prices[j] - prices[i]
+//         if (prices[i] < prices[j] &  max_profit< profit )
+//         {
+//             max_profit = profit
 
+//         }
+//     }
+// }
+
+// console.log(max_profit)
+
+
+// optimize way with one loop 
+prices = [7,1,5,3,6,4]
+// prices = [2,4,1]
+// prices = [2, 10, 1, 5]
+let max_profit = 0
+let min_value = prices[0]
+
+for (let i = 0; i < prices.length; i++)
+{
+    if(prices[i] < min_value)
+    {
+        min_value = prices[i]
+    }
+    let profit = prices[i] - min_value
+
+    if (profit > max_profit)
+    {
+        max_profit = profit 
+    }
+
+}
 console.log(max_profit)
